28886fbc0f36bf070a538bbcd284f4ec079d939e
Meet Patel <146736167+meet1412@users.noreply.github.com>
2024-09-05 14:32:04 -0400

    Initial commmit

---
+++ todo_list/home/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, HttpResponse
+from home.models import Task
+
+# Create your views here.
+def home(request):
+            
+    if request.method == "POST":
+        task = request.POST['task']
+        desc = request.POST['desc']
+        ins = Task(taskTitle=task, taskDesc=desc)
+        ins.save()
+        context = {'success': True}
+    else:
+        context = {'success': False}
+        
+    return render(request, 'index.html', context)
+
+def task(request):
+    allTask = Task.objects.all()
+    context = {'task': allTask}
+    return render(request, 'task.html', context)
